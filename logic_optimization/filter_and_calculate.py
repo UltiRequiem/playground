d6ec1454426ccfb050bb511439264a856ebdb60d
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-03-27 13:52:17 -0500

    ✨ Update

--- logic_optimization/filter_and_calculate.py
+++ logic_optimization/filter_and_calculate.py
@@ -1,4 +1,4 @@
-from functools import reduce
+import functools
 
 class Traveler():
     def __init__(self,_name, _place,_count):
@@ -20,31 +20,28 @@ def remove_duplicate_places():
     seen = []
     return list(filter(lambda x: seen.append(x.place) is None if x.place not in seen else False, lista))
 
-def get_total_visits_place_two(place):
+def get_total_visits_place(place):
     return list(filter(lambda item: item.place == place, lista))
 
-build_total = lambda x, y: x + y.count
- 
+def get_total(a, b):
+    try:
+      return a.count + b.count
+    except:
+      return a + b.count
 
-def get_toal_visits(place_list):
-    total = reduce(build_total, place_list)
-    props = ""
-    for item in place_list:
-      #  total += item.count
-        if props == "":
-            props = item.name
-        else:
-            props = props+", "+item.name
-
-    return total, props
-
+def get_names(a, b):
+    try:
+      return a.name +", "+ b.name
+    except:
+      return a +", "+ b.name
 
 def run():
     places = remove_duplicate_places()
     for place in places:
-        var = get_total_visits_place_two(place.place)
-        var_2,var_3 = get_toal_visits(var)
-        print(var_3,"han ido a",place.place, var_2,"veces.")
+        var = get_total_visits_place(place.place)
+        total = var[0].count if len(var) == 1 else functools.reduce(get_total, var)
+        names = var[0].name if len(var) == 1 else functools.reduce(get_names,var)  
+        print(names,"han ido a",place.place, total,"veces.")
 
 if __name__ == '__main__':
     run()
