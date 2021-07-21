963d67adc8fdecc32b192f5af509257559428b04
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-07-21 10:50:36 -0500

    :art: Update

---
+++ javascript/os/cp.js
@@ -0,0 +1,7 @@
+const { copyFile } = require('fs');
+
+function copy(file, newPath) {
+  copyFile(file.replace('~', process.env.HOME), newPath, (err) =>
+    err ? console.error(err) : console.log('Done!')
+  );
+}
--- python/extra/print.py
+++ python/extra/print.py
@@ -9,5 +9,4 @@ def print_100():
     for i in range(1, 100):
         print(i)
 
-
 # for i in range(10): print(i)
