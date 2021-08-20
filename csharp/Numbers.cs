06fcac25941f3a313d49edabfe2a4b3f915747a4
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-20 13:31:47 -0500

    :sparkles: Numbers in CSharp

---
+++ csharp/Numbers.cs
@@ -0,0 +1,13 @@
+namespace Numbers
+{
+    class Run
+    {
+        static void Main(string[] args)
+        {
+            int a = 18;
+            int b = 6;
+            int c = a + b;
+            System.Console.WriteLine(c);
+        }
+    }
+}
